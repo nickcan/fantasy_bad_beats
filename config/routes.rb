f4f1fe3ec6ba470f5d48729f4bf4ba726390af7c
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-10-30 13:45:56 -0700

    Created basic setup for controllers and seeded database with new seed.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  root 'beats#index'
+  root 'beats#all_beats'
+
+  resources :users, only: [:show, :update, :destroy] do
+    resources :beats, only: :index
+  end
+
+  resources :beats, only: [:index, :create, :destroy, :update] do
+    resources :comments, only: [:create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class UserSeeder
+  def self.seed
+    10.times do
+      User.create(name: Faker::Name.name, email: Faker::Internet.free_email, password: Faker::Internet.password)
+    end
+  end
+end
+
+class BeatSeeder
+  def self.seed
+    User.all.each do |user|
+      3.times do
+        user.beats.create(content: Faker::Lorem.sentence(5, true, 7))
+      end
+    end
+  end
+end
+
+class CommentSeeder
+  def self.seed
+    Beat.all.each do |beat|
+      beat.comments.create(content: Faker::Lorem.sentence(5, true, 7), user_id: User.all.sample.id)
+    end
+  end
+end
+
+UserSeeder.seed
+BeatSeeder.seed
+CommentSeeder.seed
