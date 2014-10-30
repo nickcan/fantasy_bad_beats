e0329318d6983215e083c425236e09dcdcfe524f
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-10-30 16:16:14 -0700

    Configured this project to use Angular.js as a front-end framework and Rails primarily as a back-end API that serves JSON.  Angular will now be handling all the routing for this project.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  root 'beats#all_beats'
+  root 'application#index'
+  get '*path' => 'application#index'
 
-  resources :users, only: [:show, :update, :destroy] do
-    resources :beats, only: :index
-  end
+  # resources :users, only: [:show, :update, :destroy] do
+  #   resources :beats, only: :index
+  # end
 
-  resources :beats, only: [:index, :create, :destroy, :update] do
-    resources :comments, only: [:create]
-  end
+  # resources :beats, only: [:index, :create, :destroy, :update] do
+  #   resources :comments, only: [:create]
+  # end
 end
