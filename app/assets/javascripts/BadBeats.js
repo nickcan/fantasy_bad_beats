7343c43a2d2ac4f40058227187fd1a63a745f9bc
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-10-31 15:45:25 -0700

    Completed functionality and view styling for creating a beat

--- app/assets/javascripts/BadBeats.js
+++ app/assets/javascripts/BadBeats.js
@@ -2,6 +2,7 @@ BadBeats = {};
 
 BadBeats.application = function() {
   BadBeats.View.hideComments();
+  BadBeats.View.hideBeatForm();
   BadBeats.Lightbox.hideLightbox();
   var controller = new BadBeats.Controller();
   controller.bindEvents();
--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -5,5 +5,13 @@ BadBeats.View = {
 
   toggleComments: function(element) {
     $(element).parents('.beat_content').siblings('.comments_container').slideToggle(500);
+  },
+
+  hideBeatForm: function() {
+    $('#new_beat').hide();
+  },
+
+  toggleBeatForm: function() {
+    $('#new_beat').slideToggle('slow')
   }
 };
