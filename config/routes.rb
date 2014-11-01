3def80d109a5b1b2b7805493e0da9ef79addaef4
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-11-01 01:10:46 -0700

    Completed delete functionality for comments.  Need to implement creation next.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   root 'beats#index'
 
-  resources :users, only: [:show, :create, :update, :destroy] do
-    resources :beats, only: :index
-  end
+  resources :users, only: [:show, :create, :update, :destroy]
 
   resources :beats, only: [:index, :create, :destroy, :edit, :update] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
 
   get "/login" => "sessions#new"
