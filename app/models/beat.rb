4653d6a4a5141698e8fb42bc617622abe25fe9f5
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-11-01 13:32:40 -0700

    Created votes in the database using a polymorphic association

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -1,4 +1,5 @@
 class Beat < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  has_many :votes, as: :voteable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :beats
   has_many :comments
+  has_many :votes
 
   validates :email, :password, presence: true
   validates :email, uniqueness: true
