5d858dd6d218216d308222482c26733831211207
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air-2.local>
2014-11-02 01:25:18 -0700

    Made comments and new beat form hidden using css with "display: none" upon page load in attempt to make Heroku more optimized. (vs. having them hidden with JS in doc.ready closure)

--- app/assets/javascripts/BadBeats.js
+++ app/assets/javascripts/BadBeats.js
@@ -2,8 +2,6 @@ BadBeats = {};
 
 BadBeats.application = function() {
   BadBeats.Lightbox.hideLightbox();
-  BadBeats.View.hideComments();
-  BadBeats.View.hideBeatForm();
   $('#accordion').accordion({collapsible: true, active: false});
   var controller = new BadBeats.Controller();
   controller.bindEvents();
--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -1,14 +1,6 @@
 BadBeats.View = {
-  hideComments: function() {
-    $('.comments_container').hide();
-  },
-
   toggleComments: function(element) {
     $(element).parents('.beat_content').siblings('.comments_container').slideToggle(500);
-  },
-
-  hideBeatForm: function() {
-    $('#new_beat').hide();
   },
 
   toggleBeatForm: function() {
